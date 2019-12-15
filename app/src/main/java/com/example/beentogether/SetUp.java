a6125e0ff13014944a757b024ddc1d53133188b4
PhanTrongDuc <trongduc12a1@gmail.com>
2019-12-15 23:22:30 +0700

    Fix-Setup

--- app/src/main/java/com/example/beentogether/SetUp.java
+++ app/src/main/java/com/example/beentogether/SetUp.java
@@ -12,7 +12,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class SetUp extends AppCompatActivity {
     EditText edtLDay, edtLMonth, edtLYear, edtEvent;
-    FloatingActionButton fbtBack;
+    FloatingActionButton fbtBack, fbtAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +23,13 @@ public class SetUp extends AppCompatActivity {
         edtLYear = findViewById(R.id.edt_lYear);
         edtEvent=findViewById(R.id.edt_event);
         fbtBack = findViewById(R.id.fbt_back);
+        fbtAdd=findViewById(R.id.fab_done);
 
         fbtBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SetUp.this, MainActivity.class);
-                Intent intent1= new Intent(SetUp.this, InformationLove.class);
                 Bundle bundle = new Bundle();
-                Bundle bundle1= new Bundle();
                 while (edtLDay.getText().toString()== null || edtLMonth.getText().toString() == null || edtLYear.getText().toString() == null) {
                     Toast.makeText(SetUp.this, "Bạn chưa nhập ngày bắt đầu yêu", Toast.LENGTH_SHORT).show();
                     finish();
@@ -38,15 +37,24 @@ public class SetUp extends AppCompatActivity {
                 bundle.putString("day", edtLDay.getText().toString());
                 bundle.putString("month", edtLMonth.getText().toString());
                 bundle.putString("year", edtLYear.getText().toString());
-
-                bundle1.putString("event", edtEvent.getText().toString());
-                intent1.putExtra("setup",bundle1);
                 intent.putExtra("setup", bundle);
                 setResult(1, intent);
+                finish();
+            }
+        });
+
+        fbtAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1= new Intent(SetUp.this, InformationLove.class);
+                Bundle bundle1= new Bundle();
+                bundle1.putString("event", edtEvent.getText().toString());
+                intent1.putExtra("setup",bundle1);
                 setResult(4,intent1);
                 finish();
             }
         });
+
     }
 
 
